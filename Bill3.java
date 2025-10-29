512ac4d302200627fc45eb0fc76a121ac624d731
Noameg123 <noagiuia.meghnagi@post.runi.ac.il>
2025-10-30 00:40:20 +0200

    final project

--- Bill3.java
+++ Bill3.java
@@ -7,9 +7,8 @@ public class Bill3 {
 		String name2 = args[1];
 		String name3 = args[2];
 		int sum = Integer.parseInt(args[3]); //המרה בסטרינג לאינט
-		double average = sum / 3;
-		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + average + " " + "Shekels each." );
-	    // Replace this comment with the rest of your code  
+		double average = Math.ceil(sum / 3.0);
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + average + " " + "Shekels each." );
 		//% java Bill3 Ron Lisa Dan 100 Dear Dan, Lisa, and Ron: pay 34.0 Shekels each. 
 	}
 }
